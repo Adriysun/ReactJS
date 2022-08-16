4843357df90b1336482533702af52693aac5db93
Adrian <105469229+Adriysun@users.noreply.github.com>
2022-08-16 19:28:56 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-function App(props) {
-  var situacao = ""
+import React from 'react';
+import Card from './components/Card/Card';
+import Exemplo01 from './components/Exemplo01.js';
+import NumeroAleatorio from './components/NumeroAleatorio';
+import Infos from './components/Infos';
+import Gostos from './components/Gostos';
+import "./App.css";
+import ExibeNotas from './components/ExibeNotas';
 
-  if (props.nota >=5)
-   situacao = "Aprovado"
-  else
-   situacao = "Reprovado"
 
-  return (
+
+export default function App() {
+return(
     <div className="App">
-     <h1>Teste ReactJS</h1>
-     <h2>Seja bem-vindo {props.primeiroNome} {props.segundoNome} </h2>
-     <h2>Sua média final é: {props.nota}</h2>
-     <h2>Situação: {situacao}</h2>
-     
+      
+      <h1> Exemplos com React</h1>
+      <div className="cards">
+      <Card titulo="Título">
+          <Exemplo01 nome="Adrian" idade="20"/>          
+      </Card>
+
+      <Card titulo="NumeroAleatorio">
+        <NumeroAleatorio min={ 20 } max={ 40 } />
+      </Card>
+
+      <Card titulo="Info">
+        <Infos escola="Cotuca" curso="DS"> </Infos>
+      </Card>
+
+      <Card titulo="Gostos">
+        <Gostos gostos="Jogar"> </Gostos>
+      </Card>
+
+      <Card titulo="Comunicação direta">
+        <ExibeNotas> </ExibeNotas>
+      </Card>
+
     </div>
+</div>
   );
 }
-
-export default App;
 
 // return apenas retorna UM elemento, como o exemplo da DIV acima, se
 // for retirado a DIV, o return não pode retornar e funciona apenas
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App primeiroNome="Adrian" segundoNome="Rodrigues" nota="7.0"/>
-    
-  
-  </React.StrictMode>
-);
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+  );
 
